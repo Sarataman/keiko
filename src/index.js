843995a5fae7b28508a685640eb45441af2e9cc6
Sarah <sarahehi.sa@gmail.com>
2020-12-02 04:17:37 +0100

    Added sections

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel 
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
+function navToggle() {
+    const navs =  document.querySelectorAll('.nav-item')
+    navs.forEach(nav => nav.classList.toggle('toggle_Show'));
+}
+document.querySelector('.nav-toggle').addEventListener('click', navToggle);
